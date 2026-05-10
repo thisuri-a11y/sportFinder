a79bad31750817f1a294ec317036ea68cc391408
thisuri-a11y <mandirathisuri@gmail.com>
2026-05-10 08:28:49 +0530

    initialize server setup with Express, Mongoose, and routes for shop and product

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 //test
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+import shopRoute from "./Route/shopRoute.js";
+import productRoute from "./Route/productRoute.js";
+
+const app = express();
+
+app.use(bodyParser.json());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose.connect(MONGOURL)
+.then(()=>{
+
+    console.log("Database connected successfully.");
+
+    app.listen(PORT,()=>{
+
+        console.log(`Server is running on port : ${PORT}`);
+
+    });
+
+})
+.catch((error)=> console.log(error));
+
+app.use("/api/shop",shopRoute);
+app.use("/api/product",productRoute);
